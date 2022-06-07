3f30cfd3767e815271d76a16024d830600a83cd5
Purwa Sadr <purwasadr@gmail.com>
2022-06-07 17:17:24 +0700

    feat : change font style to open sans

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
+      </Head>
+      <body className='font-openSans'>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
