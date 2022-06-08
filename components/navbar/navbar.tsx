1e397a83df09b6d6e052a0f177c2a7f9ea896597
Purwa Sadr <purwasadr@gmail.com>
2022-06-08 14:29:06 +0700

    refactor : sort className

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -5,9 +5,9 @@ import NavbarLink from './navbar-link';
 
 const Navbar: FC = () => {
     return (
-        <nav className="sticky top-0 z-10 bg-white border-gray-200 px-2 sm:px-4 py-2.5 border-b dark:bg-gray-800">
+        <nav className="sticky top-0 z-10 px-2 sm:px-4 py-2.5 border-b border-gray-200 bg-white dark:bg-gray-800">
             <div className="flex flex-wrap justify-between items-center px-3 lg:px-4 mx-auto">
-                <div className="items-center flex w-auto">
+                <div className="flex items-center w-auto">
                     <Link href="/dashboard/posts">
                         <div className="flex items-center">
                             <span className="self-center text-xl font-regular font-monumentExtended whitespace-nowrap dark:text-white">
@@ -17,7 +17,7 @@ const Navbar: FC = () => {
                     </Link>
                 </div>
                 <div className="flex items-center">
-                    <ul className="flex flex-row pl-4 text-sm font-medium">
+                    <ul className="flex flex-row flex-wrap pl-4 text-sm font-semibold">
                         <NavbarLink href='#' caption="Home" />
                         <NavbarLink href="#" caption="Organization" isDropdown={true}/>
                         <NavbarLink href="#" caption="Division"/>
