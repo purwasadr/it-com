ed587668ba822a22ec58d8261bc045aed33d5895
Purwa Sadr <purwasadr@gmail.com>
2022-06-11 14:31:43 +0700

    feat : change main font to Poppins

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       </Head>
-      <body className='font-openSans'>
+      <body className='font-poppins'>
         <Main />
         <NextScript />
       </body>
