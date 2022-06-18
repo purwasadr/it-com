f18473766b8cc5d1672507ef5c0425c4a5cbb427
Purwa Sadr <purwasadr@gmail.com>
2022-06-18 11:19:40 +0700

    feat: add libs for showing infinite scrolling

---
+++ libs/useEventPaging.ts
@@ -0,0 +1,51 @@
+import qs from 'qs';
+import React, { useState, useEffect } from "react";
+import { fetchGet } from './fetch';
+import { responseToEvents } from './transform';
+
+function useEventPaging(pageNum: number) {
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [events, setEvents] = useState<any[]>([]);
+  const [hasMore, setHasMore] = useState(false);
+
+  useEffect(() => {
+    const qEvents = qs.stringify({
+        populate: '*',
+        pagination: {
+            page: pageNum,
+            pageSize: 5,
+        }
+    });
+
+    const abortController = new AbortController()
+    
+    setIsLoading(true);
+    setError(false);
+
+    fetchGet(process.env.NEXT_PUBLIC_BACKEND_API +
+        `/api/events?${qEvents}`, { signal:  abortController.signal })
+        .then(res => res.json())
+      .then((res) => {
+        const trRes = responseToEvents(res);
+        setEvents((prev: any[]) => {
+          // return [...prev, ...trRes.data];
+          return Array.from(new Map([...prev, ...trRes.data].map(item => [item['id'], item])).values());
+        });
+        setHasMore(trRes.meta.pagination.page < trRes.meta.pagination.pageCount);
+        setIsLoading(false);
+      })
+      .catch((err: any) => {
+        
+        setError(err);
+      });
+
+    return () => {
+        abortController.abort();
+    }
+  }, [pageNum]);
+
+  return { isLoading, error, events, hasMore };
+}
+
+export default useEventPaging;
