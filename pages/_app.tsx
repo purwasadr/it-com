2c9040844dcc9be6fdaa94d6b313837bcf490d46
Purwa Sadr <purwasadr@gmail.com>
2022-06-22 09:59:33 +0700

    feat: add layout

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,16 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type {AppProps} from 'next/app';
+import Primary from '@/components/layouts/primary';
+import { useRouter } from 'next/router';
 
-function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+function MyApp({Component, pageProps}: AppProps) {
+    const router = useRouter();
+
+    return (
+        <Primary noPadding={router.pathname === '/'}>
+            <Component {...pageProps} />
+        </Primary>
+    );
 }
 
-export default MyApp
+export default MyApp;
