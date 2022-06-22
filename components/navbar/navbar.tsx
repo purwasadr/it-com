d850e54558635739981f48f8e0bed0a3a1d08215
Purwa Sadr <purwasadr@gmail.com>
2022-06-22 09:59:09 +0700

    feat: remove organization menu

--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,6 +1,6 @@
 import {Menu} from '@headlessui/react';
 import Link from 'next/link';
-import { forwardRef, ForwardRefRenderFunction, Ref } from 'react';
+import { forwardRef } from 'react';
 import NavbarLink from './navbar-link';
 
 interface PropsMenuLink {
@@ -33,24 +33,19 @@ const Navbar = () => {
 
     return (
         <nav className="sticky top-0 z-10 px-2 sm:px-4 py-2.5 border-b border-gray-200 bg-white dark:bg-gray-800">
-            <div className="flex flex-wrap justify-between items-center px-3 lg:px-4 mx-auto">
+            <div className="flex flex-wrap max-w-[90rem] justify-between items-center px-3 lg:px-4 mx-auto">
                 <div className="flex items-center w-auto">
-                    <Link href="/dashboard/posts">
-                        <div className="flex items-center">
-                            <span className="self-center text-xl font-regular font-monumentExtended whitespace-nowrap dark:text-white">
+                    <Link href="/">
+                        <a className="flex items-center">
+                            <span className="text-xl font-regular font-monumentExtended whitespace-nowrap dark:text-white">
                                 IT COM
                             </span>
-                        </div>
+                        </a>
                     </Link>
                 </div>
                 <div className="hidden md:flex md:items-center">
                     <ul className="flex flex-row flex-wrap pl-4 text-sm font-medium">
                         <NavbarLink href="/" caption="Home" />
-                        <NavbarLink
-                            href="#"
-                            caption="Organization"
-                            isDropdown={true}
-                        />
                         <NavbarLink href="/divisions" caption="Division" />
                         <NavbarLink href="#" caption="Contact" />
                     </ul>
@@ -58,28 +53,12 @@ const Navbar = () => {
                 <div className="md:hidden relative">
                     <Menu as="div">
                         <Menu.Button className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
-                            <svg
-                                className="w-6 h-6"
-                                fill="currentColor"
-                                viewBox="0 0 20 20"
-                                xmlns="http://www.w3.org/2000/svg"
-                            >
-                                <path
-                                    fillRule="evenodd"
-                                    d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-                                    clipRule="evenodd"
-                                ></path>
-                            </svg>
+                            <svg className="w-6 h-6" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"><circle cx="12" cy="2" r="2"/><circle cx="12" cy="12" r="2"/><circle cx="12" cy="22" r="2"/></svg>
                         </Menu.Button>
                         <Menu.Items className="absolute right-0 mt-2 w-56 overflow-y-hidden divide-y divide-gray-100 rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
                             <Menu.Item>
                                 {({active}) => (
                                     <MenuLink active={active} href="/" >Home</MenuLink>
-                                )}
-                            </Menu.Item>
-                            <Menu.Item>
-                                {({active}) => (
-                                    <MenuLink active={active} href="#">Organization</MenuLink>
                                 )}
                             </Menu.Item>
                             <Menu.Item>
