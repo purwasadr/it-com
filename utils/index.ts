29ad669552d7cdc99886e1afc30b59be6364a984
Purwa Sadr <purwasadr@gmail.com>
2022-06-24 14:38:38 +0700

    docs: add docs

--- utils/index.ts
+++ utils/index.ts
@@ -1,8 +1,12 @@
+// Mengebalikan object yang telah dibersihkan dari properti yang
+// bertipe undefined
 export const removeUndefined = <T,>(props: T): T =>
   Object.fromEntries(
     Object.entries(props).filter(([, value]) => value !== undefined),
   ) as T;
 
+// Mengembalikan string jika semua nilai bertype string, mengembalikan
+// undefined jika salah satu nilai bertype undefined 
 export const concatOrUnd = (...strings: Array<string | undefined>) => {
     let result: string = '';
 
