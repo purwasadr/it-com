2c4e684435cbe6902388bc178960c4aa4824c3ef
Purwa Sadr <purwasadr@gmail.com>
2022-06-24 14:29:47 +0700

    refactor: add alternate code

--- libs/fetch.ts
+++ libs/fetch.ts
@@ -1,7 +1,3 @@
-interface FetchInit {
-    headers?: HeadersInit | undefined,
-    body?: BodyInit | null | undefined
-}
 
 function doFetch(url: string, method: string, init: RequestInit = { headers: {}, body: null }) {
     const { headers, body } = init;
@@ -16,11 +12,62 @@ function doFetch(url: string, method: string, init: RequestInit = { headers: {},
             body: body ? JSON.stringify(body) : null
         });
 }
+function doFetch2(url: string, method: string, init: RequestInit = { headers: {}, body: null }) {
+    const { headers, body } = init;
+    
+    return new Promise<Response>((resolve, reject) => {
+        fetch(url, {
+            ...init,
+            method,
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: body ? JSON.stringify(body) : null
+        }).then((res) => {
+            if (!res.ok) reject(res.statusText);
+            resolve(res);
+        }).catch((reason) => {
+            reject(reason)
+        });
+    });
+}
+
+interface ResponseData extends Response {
+    data: any
+}
+
+function doFetch3(url: string, method: string, init: RequestInit = { headers: {}, body: null }) {
+    const { headers, body } = init;
+    
+    return new Promise<ResponseData>((resolve, reject) => {
+        fetch(url, {
+            ...init,
+            method,
+            headers: {
+                ...headers,
+                'Content-Type': 'application/json'
+            },
+            body: body ? JSON.stringify(body) : null
+        }).then(async (res) => {
+            if (!res.ok) reject(res.statusText);
+            return {
+                ...res,
+                data: await res.json() 
+            }
+        }).then((res) => {
+            resolve(res);
+        })
+        .catch((reason) => {
+            reject(reason)
+        });
+    });
+}
 
 export function fetchPost(url: string, init?: RequestInit) {
-    return doFetch(url, 'POST', init);
+    return doFetch2(url, 'POST', init);
 }
 
 export function fetchGet(url: string, init?: RequestInit) {
-    return doFetch(url, 'GET', init);
+    return doFetch2(url, 'GET', init);
 }
