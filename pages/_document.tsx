1c373dd9489ac3c5960bd09e2491c8a1b2e3b83f
Purwa Sadr <purwasadr@gmail.com>
2022-06-24 14:16:04 +0700

    refactor: remove open sans link font

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,6 @@ export default function Document() {
   return (
     <Html>
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,400;0,500;0,600;0,700;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
       </Head>
       <body className='font-poppins'>
         <Main />
