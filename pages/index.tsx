c6b42796c8b7504c1c6bd4facc83b54d90f76fac
Purwa Sadr <purwasadr@gmail.com>
2022-06-25 14:39:27 +0700

    fix: empty event message

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,10 +2,10 @@ import CardEvent from '@/components/card/card-event';
 import type {InferGetServerSidePropsType, NextPage} from 'next';
 import Image from 'next/image';
 import {GetServerSideProps} from 'next';
-import { toEvents } from 'utils/transform';
+import {toEvents} from 'utils/transform';
 import ButtonLink from '@/components/button-link';
-import EventModel, { EventItem } from 'models/event';
-import { removeUndefined } from 'utils';
+import EventModel, {EventItem} from 'models/event';
+import {removeUndefined} from 'utils';
 
 export const getServerSideProps: GetServerSideProps = async () => {
     try {
@@ -14,15 +14,18 @@ export const getServerSideProps: GetServerSideProps = async () => {
             EventModel.getHistoryEvents(),
         ];
 
-        const [resUpcomingEvents, resHistoryEvents] = await Promise.all([res[0], res[1]]);
+        const [resUpcomingEvents, resHistoryEvents] = await Promise.all([
+            res[0],
+            res[1],
+        ]);
 
         return {
             props: removeUndefined({
                 upcomingEvents: {
-                    data: toEvents(resUpcomingEvents.data)
+                    data: toEvents(resUpcomingEvents.data),
                 },
                 historyEvents: {
-                   data: toEvents(resHistoryEvents.data)
+                    data: toEvents(resHistoryEvents.data),
                 },
             }),
         };
@@ -34,20 +37,19 @@ export const getServerSideProps: GetServerSideProps = async () => {
                     error: 'Cannot get data',
                 },
                 historyEvents: {
-                    error: 'Cannot get data'
-                }
+                    error: 'Cannot get data',
+                },
             },
         };
     }
 };
 
 interface PageProps {
-    upcomingEvents: { data?: EventItem[], error?: string };
-    historyEvents: { data?: EventItem[], error?: string };
+    upcomingEvents: {data?: EventItem[]; error?: string};
+    historyEvents: {data?: EventItem[]; error?: string};
 }
 
 const Home: NextPage<PageProps> = ({upcomingEvents, historyEvents}) => {
-    console.log('Home Page ', upcomingEvents);
 
     return (
         <>
@@ -81,24 +83,28 @@ const Home: NextPage<PageProps> = ({upcomingEvents, historyEvents}) => {
                             upcomingEvents.data?.length ? '' : ''
                         }`}
                     >
-                    {upcomingEvents.data?.map((event: any) => (
-                                <CardEvent
-                                    className="md:flex-shrink-0"
-                                    href={`/events/detail/${event.id}`}
-                                    key={event.id}
-                                    title={event.title}
-                                    poster={
-                                        process.env.NEXT_PUBLIC_BACKEND_API +
-                                        event.poster
-                                    }
-                                    eventTypes={event.eventTypes}
-                                    date={new Date(event.date).toDateString()}
-                                />
-                            ))}
-                        <article>{!upcomingEvents.data ? 'No event yet' : ''}</article>
+                        {upcomingEvents.data?.map((event: any) => (
+                            <CardEvent
+                                className="md:flex-shrink-0"
+                                href={`/events/detail/${event.id}`}
+                                key={event.id}
+                                title={event.title}
+                                poster={
+                                    process.env.NEXT_PUBLIC_BACKEND_API +
+                                    event.poster
+                                }
+                                eventTypes={event.eventTypes}
+                                date={new Date(event.date).toDateString()}
+                            />
+                        ))}
+                        {!upcomingEvents.data ? (
+                            <p className="text-center">No event yet</p>
+                        ) : (
+                            ''
+                        )}
                     </section>
                 </section>
-               
+
                 <section>
                     <h3 className="text-2xl mt-12 uppercase text-center font-monumentExtended">
                         Event History
@@ -106,25 +112,31 @@ const Home: NextPage<PageProps> = ({upcomingEvents, historyEvents}) => {
 
                     <section className="flex flex-col md:flex-row md:justify-center md:flex-wrap p-8 gap-4">
                         {historyEvents.data?.map((event: any) => (
-                                <CardEvent
-                                    className="flex-shrink-0"
-                                    href={`/events/detail/${event.id}`}
-                                    key={event.id}
-                                    title={event.title}
-                                    poster={
-                                        process.env.NEXT_PUBLIC_BACKEND_API +
-                                        event.poster
-                                    }
-                                    eventTypes={event.eventTypes}
-                                    date={new Date(event.date).toDateString()}
-                                />
-                            ))}
-                        <article className='text-center'>{!historyEvents.data ? 'No event yet' : ''}</article>
-                    </section>                    
+                            <CardEvent
+                                className="flex-shrink-0"
+                                href={`/events/detail/${event.id}`}
+                                key={event.id}
+                                title={event.title}
+                                poster={
+                                    process.env.NEXT_PUBLIC_BACKEND_API +
+                                    event.poster
+                                }
+                                eventTypes={event.eventTypes}
+                                date={new Date(event.date).toDateString()}
+                            />
+                        ))}
+                        {!historyEvents.data ? (
+                            <p className="text-center">No event yet</p>
+                        ) : (
+                            ''
+                        )}
+                    </section>
                 </section>
                 <section>
-                    <div className="flex justify-center mt-4"> 
-                        <ButtonLink href={"/events"} variant='outline'>See all events</ButtonLink>
+                    <div className="flex justify-center mt-4">
+                        <ButtonLink href={'/events'} variant="outline">
+                            See all events
+                        </ButtonLink>
                     </div>
                 </section>
                 <div className="mt-4"></div>
