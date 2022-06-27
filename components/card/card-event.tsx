1d1bb4491dd5992fd9fae0b242b04461e5183fe3
Purwa Sadr <purwasadr@gmail.com>
2022-06-27 13:45:52 +0700

    style: change date icon

--- components/card/card-event.tsx
+++ components/card/card-event.tsx
@@ -54,16 +54,14 @@ const CardEvent = ({
                         </h5>
                         <p className="inline-flex items-center mt-2 text-gray-700 dark:text-gray-400">
                             <svg
+                                className='mb-[0.3px]'
                                 xmlns="http://www.w3.org/2000/svg"
                                 viewBox="0 0 24 24"
                                 width="13"
                                 height="13"
                                 fill="currentColor"
                             >
-                                <path d="M19,2H18V1a1,1,0,0,0-2,0V2H8V1A1,1,0,0,0,6,1V2H5A5.006,5.006,0,0,0,0,7V19a5.006,5.006,0,0,0,5,5H19a5.006,5.006,0,0,0,5-5V7A5.006,5.006,0,0,0,19,2ZM2,7A3,3,0,0,1,5,4H19a3,3,0,0,1,3,3V8H2ZM19,22H5a3,3,0,0,1-3-3V10H22v9A3,3,0,0,1,19,22Z" />
-                                <circle cx="12" cy="15" r="1.5" />
-                                <circle cx="7" cy="15" r="1.5" />
-                                <circle cx="17" cy="15" r="1.5" />
+                                <path d="M18.5,2H18V1.5A1.5,1.5,0,0,0,16.5,0h0A1.5,1.5,0,0,0,15,1.5V2H9V1.5A1.5,1.5,0,0,0,7.5,0h0A1.5,1.5,0,0,0,6,1.5V2H5.5A5.5,5.5,0,0,0,0,7.5v11A5.5,5.5,0,0,0,5.5,24h13A5.5,5.5,0,0,0,24,18.5V7.5A5.5,5.5,0,0,0,18.5,2Zm0,19H5.5A2.5,2.5,0,0,1,3,18.5V10H21v8.5A2.5,2.5,0,0,1,18.5,21Z" />
                             </svg>
                             <span className="ml-[6px] text-xs font-medium">
                                 {date}
