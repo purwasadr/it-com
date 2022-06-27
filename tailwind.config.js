57192ee8c1a8bcea185730320b047341c1b0d978
Purwa Sadr <purwasadr@gmail.com>
2022-06-27 13:45:16 +0700

    chore: remove some content path

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,6 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    './layouts/**/*.{js,ts,jsx,tsx}',
-    './node_modules/flowbite/**/*.js'
   ],
   theme: {
     extend: {
