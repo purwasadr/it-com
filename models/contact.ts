7f89757b1b41f86f292cc110fcac858073e64e5b
Purwa Sadr <purwasadr@gmail.com>
2022-07-06 11:04:19 +0700

    refactor: add mapper in getContacs func

--- models/contact.ts
+++ models/contact.ts
@@ -1,14 +1,18 @@
 import { fetchGet } from 'libs/fetch';
+import { toContacts } from 'utils/transform';
 
 export interface Contact {
-    id: number,
+    id?: number,
     name?: string,
+    link?: string
 }
 
-const getContacts = () => {
-    return fetchGet(
+const getContacts = async () => {
+    const req = await fetchGet(
         process.env.NEXT_PUBLIC_BACKEND_API + `/api/contacts`
     ).then((res) => res.json());
+
+    return toContacts(req.data);
 }
 
 const ContactModel = {
