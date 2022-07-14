6b014c3948d9c65ec16d0cd9fc7820b57fa7a176
Purwa Sadr <purwasadr@gmail.com>
2022-07-14 08:52:08 +0700

    refactor: add mapper in fetch func

--- models/event.ts
+++ models/event.ts
@@ -1,6 +1,6 @@
 import {fetchGet} from 'libs/fetch';
 import qs from 'qs';
-import { toEvent } from 'utils/transform';
+import { toEvent, toEvents } from 'utils/transform';
 
 export interface Event {
     id?: number;
@@ -35,15 +35,23 @@ const qEvents = (operator: string) =>
         }
     });
 
-const getUpcomingEvents = () =>
-    fetchGet(
+const getUpcomingEvents = async () => {
+    const req = await fetchGet(
         process.env.NEXT_PUBLIC_BACKEND_API + `/api/events?${qEvents('$gt')}`
     ).then((res) => res.json());
 
-const getHistoryEvents = () =>
-    fetchGet(
+    return toEvents(req.data)
+}
+    
+
+const getHistoryEvents = async () => {
+    const req = await fetchGet(
         process.env.NEXT_PUBLIC_BACKEND_API + `/api/events?${qEvents('$lt')}`
     ).then((res) => res.json());
+
+    return toEvents(req.data)
+}
+    
 
 const getEvent = async (slug: string) => {
     const qEvent = qs.stringify({
