b514518487aeec738b4403ac95b36a8e40c4f110
Purwa Sadr <purwasadr@gmail.com>
2022-07-18 14:37:04 +0700

    feat: add progressbar when change page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css';
 import type {AppProps} from 'next/app';
 import Primary from '@/components/layouts/primary';
 import { useRouter } from 'next/router';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({Component, pageProps}: AppProps) {
     const router = useRouter();
 
     return (
         <Primary noPadding={router.pathname === '/'}>
+            <NextNProgress height={2} options={{ showSpinner: false }}/>
             <Component {...pageProps} />
         </Primary>
     );
