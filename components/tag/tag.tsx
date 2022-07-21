97a519ed71101dc1a607cd88a13c66ec8008cee4
Purwa Sadr <purwasadr@gmail.com>
2022-07-21 20:24:22 +0700

    refactor: change font size

--- components/tag/tag.tsx
+++ components/tag/tag.tsx
@@ -4,7 +4,7 @@ interface Props {
 
 const Tag = ({caption}: Props) => {
     return (
-        <div className="px-2 py-1 flex rounded-md w-fit bg-purple-200 text-purple-900 font-medium text-[0.7rem]">
+        <div className="px-2 py-1 flex rounded-md w-fit bg-purple-200 text-purple-900 font-medium text-xs">
             {caption}
         </div>
     );
