93c9cd8241eb079d367c753fa1d65ace1c67bc07
Purwa Sadr <purwasadr@gmail.com>
2022-07-21 20:25:00 +0700

    change: change font to General Sans

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,8 +4,9 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <link href="https://api.fontshare.com/v2/css?f[]=general-sans@701,200,500,301,201,300,601,600,401,501,400,700&display=swap" rel="stylesheet" />
       </Head>
-      <body className="font-poppins text-slate-900">
+      <body className="font-general-sans text-slate-900">
         <Main />
         <NextScript />
       </body>
