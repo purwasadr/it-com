73a06709051d878deec43e5e072c056e92025c24
Purwa Sadr <purwasadr@gmail.com>
2022-07-23 16:19:41 +0700

    refactor: change hover and focus color

--- components/button.tsx
+++ components/button.tsx
@@ -20,14 +20,14 @@ const Button = ({
 }: Props) => {
     const variantClas =
         variant === 'fill'
-            ? 'bg-blue-700 text-white hover:bg-blue-900'
-            : 'text-slate-900 bg-white border border-gray-300 shadow-sm hover:bg-gray-100';
+            ? 'bg-blue-700 text-white hover:bg-blue-800 focus:ring-blue-500'
+            : 'text-slate-900 bg-white border border-gray-300 shadow-sm hover:bg-gray-100 focus:ring-slate-200';
 
     return (
         <button
             type={type}
             className={
-                `${variantClas} focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-slate-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 ${buttonClassDark} ${
+                `${variantClas} focus:outline-none focus:ring-2 focus:ring-offset-2 font-medium rounded-lg text-sm px-5 py-2.5 ${buttonClassDark} ${
                     disabled && 'opacity-25'
                 } ` + className
             }
