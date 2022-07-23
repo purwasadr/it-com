916c4ec184dbcdfbb0a848adb42d79e09f7f4d3a
Purwa Sadr <purwasadr@gmail.com>
2022-07-23 16:25:45 +0700

    change: full change style

--- pages/events/detail/[slug].tsx
+++ pages/events/detail/[slug].tsx
@@ -45,9 +45,9 @@ const EventDetail: NextPage<PageProps> = ({event}) => {
                 <title key="title">{`${event.data?.title} | IT Com`}</title>
                 <meta name="description" content={event.data?.description} key="description" />
             </Head>
-            <section className="flex flex-col md:flex-row flex-nowrap md:space-x-10">
-                <section className="w-full md:w-4/6">
-                    <figure className="relative w-full h-[200px] sm:h-[250px] lg:h-[300px] rounded-lg shadow-md overflow-hidden">
+            <section className="flex flex-col md:flex-row flex-nowrap md:space-x-8">
+                <section className="w-full md:w-[70%]">
+                    <figure className="relative w-full h-[200px] sm:h-[250px] lg:h-[300px] rounded-md md:rounded-lg shadow-sm md:shadow-md overflow-hidden">
                       {event.data?.poster && (<Image
                             src={process.env.NEXT_PUBLIC_BACKEND_API + event.data.poster}
                             alt="Poster Event"
@@ -56,37 +56,37 @@ const EventDetail: NextPage<PageProps> = ({event}) => {
                         />)}
                     </figure>
                 </section>
-                <section className="w-full md:w-2/6 mt-4 md:mt-0 md:py-4">
-                    <h3 className="text-xl font-medium text-slate-900">
+                <section className="w-full md:w-[30%] mt-4 md:mt-0 p-4 md:p-6 bg-white rounded-md md:rounded-lg shadow-sm md:shadow-md">
+                    <h2 className="text-slate-900 text-lg md:text-xl line-clamp-3">
                         {event.data?.title}
-                    </h3>
-                    <section className="mt-4">
+                    </h2>
+                    <section className="mt-4 md:mt-7">
                         <article className="flex items-center text-gray-500 dark:text-gray-400">
                             <DateIcon className='text-blue-700 h-[16px] w-[16px]' />
-                            <span className="ml-2 text-sm font-medium text-slate-900">
+                            <span className="ml-4">
                                 {getDateShort(event.data?.date) ?? '-'}
                             </span>
                         </article>
                         <article className="flex items-center mt-2 text-gray-500 dark:text-gray-400">
                             <LocationIcon className="text-blue-700 h-[16px] w-[16px]" />
-                            <span className="ml-2 text-sm font-medium text-slate-900">
+                            <span className="ml-4">
                                 {event.data?.location}
                             </span>
                         </article>
                     </section>
                 </section>
             </section>
-            <section className="flex flex-col md:flex-row flex-nowrap md:space-x-10 mt-4">
-                <section className="w-full md:w-4/6">
-                    <h3 className="text-lg font-medium">Event Description</h3>
-                    <p className="mt-2 text-sm text-slate-700 whitespace-pre-line">
+            <section className="flex flex-col md:flex-row flex-nowrap md:space-x-8 mt-4 md:mt-6">
+                <section className="w-full md:w-[70%] p-4 md:p-5 bg-white rounded-md md:rounded-lg shadow-sm md:shadow-md ">
+                    <h3 className="text-base md:text-lg">Description</h3>
+                    <p className="mt-2 text-slate-500 whitespace-pre-line">
                         {event.data?.description}
                     </p>
                 </section>
-                <section className="w-full md:w-2/6 mt-5 md:mt-0">
+                <section className="w-full md:w-[30%] mt-4 md:mt-0">
                     {event.data?.registerLink && 
                     (<a className="" href={event.data?.registerLink} target="_blank" rel="noreferrer">
-                        <Button className="w-full">Register</Button>
+                        <Button className="w-full" variant="fill">Register</Button>
                     </a>)} 
                 </section>
             </section>
