6570b6f7f031f875dabd972938379a77de9a62b6
Purwa Sadr <purwasadr@gmail.com>
2022-07-23 16:26:47 +0700

    change: change bg color

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
       <Head>
         <link href="https://api.fontshare.com/v2/css?f[]=general-sans@701,200,500,301,201,300,601,600,401,501,400,700&display=swap" rel="stylesheet" />
       </Head>
-      <body className="font-general-sans text-slate-900">
+      <body className="font-general-sans text-slate-900 bg-gray-50">
         <Main />
         <NextScript />
       </body>
