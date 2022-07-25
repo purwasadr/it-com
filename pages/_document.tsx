d7d50afe52ebb592e60a116646516d7bf3d92790
Purwa Sadr <purwasadr@gmail.com>
2022-07-25 09:20:07 +0700

    refactor: add google site verification meta

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,7 @@ export default function Document() {
   return (
     <Html>
       <Head>
+        <meta name="google-site-verification" content="k-9dmncBHnL9h8G4Sfg9cPZC1Wu864hlkNjRqo2SHSY" />
         <link href="https://api.fontshare.com/v2/css?f[]=general-sans@701,200,500,301,201,300,601,600,401,501,400,700&display=swap" rel="stylesheet" />
       </Head>
       <body className="font-general-sans text-slate-900 bg-gray-100">
