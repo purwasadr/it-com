949f45f26f46407765006ae0a5549681e10fd54f
Purwa Sadr <purwasadr@gmail.com>
2022-08-01 16:50:44 +0700

    refactor: change height NextNProgress

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,7 @@
 import '../styles/globals.css';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+import 'react-multi-carousel/lib/styles.css';
 import type {AppProps} from 'next/app';
 import Primary from '@/components/layouts/primary';
 import { useRouter } from 'next/router';
@@ -9,7 +12,7 @@ function MyApp({Component, pageProps}: AppProps) {
 
     return (
         <Primary noPadding={router.pathname === '/'}>
-            <NextNProgress height={2} options={{ showSpinner: false }}/>
+            <NextNProgress height={3} options={{ showSpinner: false }}/>
             <Component {...pageProps} />
         </Primary>
     );
