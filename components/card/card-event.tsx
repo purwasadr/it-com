dd2eb19c604842626b4d7da03dfb9723cd038f52
Purwa Sadr <purwasadr@gmail.com>
2022-08-01 16:49:35 +0700

    refactor: change tag structure

--- components/card/card-event.tsx
+++ components/card/card-event.tsx
@@ -23,43 +23,43 @@ const CardEvent = ({
     href = '#',
 }: Props) => {
     return (
-        <Link href={href} >
-            <a>
-                <div
-                    ref={inputRef}
-                    className={`w-full h-full md:w-[260px] bg-white rounded-md border shadow-sm border-gray-300 hover:bg-gray-100 cursor-pointer overflow-hidden dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ${className}`}
-                >
-                    <div className="relative aspect-[4/3]">
-                    {poster ? (<Image
+        <Link href={href}>
+            <a
+                className={`block w-full h-full bg-white rounded-md border shadow-sm border-gray-300 hover:bg-gray-100 cursor-pointer overflow-hidden focus:outline-none dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700 ${className}`}
+                ref={inputRef}
+            >
+                <div className="relative aspect-[4/3]">
+                    {poster ? (
+                        <Image
                             src={poster}
                             layout="fill"
                             alt="Poster"
                             objectFit="cover"
-                        />) : (
-                            <div className="p-10 w-full h-full bg-gray-500">
-                                <PictureIcon className="w-full h-full text-gray-400" />
-                            </div>
-                        )}   
-                    </div>
-                    <div className="py-4 px-3.5">
-                        {getOrNull(eventTypes?.length) && (
-                            <div className="flex flex-wrap gap-2 mb-3.5">
-                                {eventTypes?.map((eventType) => (
-                                    <Tag
-                                        key={eventType.id}
-                                        caption={eventType.name}
-                                    />
-                                ))}
-                            </div>
-                        )}
+                        />
+                    ) : (
+                        <div className="p-10 w-full h-full bg-gray-500">
+                            <PictureIcon className="w-full h-full text-gray-400" />
+                        </div>
+                    )}
+                </div>
+                <div className="py-4 px-3.5">
+                    {getOrNull(eventTypes?.length) && (
+                        <div className="flex flex-wrap gap-2 mb-3.5">
+                            {eventTypes?.map((eventType) => (
+                                <Tag
+                                    key={eventType.id}
+                                    caption={eventType.name}
+                                />
+                            ))}
+                        </div>
+                    )}
 
-                        <h5 className="font-semibold text-base capsize text-gray-900 dark:text-white">
-                            <span className="line-clamp-2">{title}</span>
-                        </h5>
-                        <p className="mt-4 font-medium text-slate-500 dark:text-gray-400 text-xs capsize">
-                            {date}
-                        </p>
-                    </div>
+                    <h5 className="font-semibold text-base capsize text-gray-900 dark:text-white">
+                        <span className="line-clamp-2">{title}</span>
+                    </h5>
+                    <p className="mt-4 font-medium text-slate-500 dark:text-gray-400 text-xs capsize">
+                        {date}
+                    </p>
                 </div>
             </a>
         </Link>
