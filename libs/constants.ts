93f5d558d0a2055cf41f73e801f301391b37a774
Purwa Sadr <purwasadr@gmail.com>
2022-08-06 20:14:16 +0700

    refactor: add API_URL constant

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,2 +1,3 @@
+export const APP_URL = process.env.NEXT_PUBLIC_APP_URL;
 export const BACKEND_MEDIA_PREFIX = process.env.NEXT_PUBLIC_BACKEND_MEDIA_PREFIX;
 export const BACKEND_API = process.env.NEXT_PUBLIC_BACKEND_API;
