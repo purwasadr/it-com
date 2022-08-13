148b4e5e9bbe35ee4db159ee4b8a3d3ccdc8bd64
Purwa Sadr <purwasadr@gmail.com>
2022-08-13 20:28:51 +0700

    refactor: change fetch method

--- pages/divisions/detail/[slug].tsx
+++ pages/divisions/detail/[slug].tsx
@@ -1,91 +1,74 @@
 import CardDivisionMember from '@/components/card/card-division-member';
-import { BACKEND_MEDIA_PREFIX } from 'libs/constants';
-import {fetchGet} from 'libs/fetch';
+import DivisionModel, { Division } from 'models/division';
+import DivisionMemberModel, { DivisionMemberItem } from 'models/division-member-model';
 import {GetServerSideProps, NextPage} from 'next';
 import Head from 'next/head';
-import qs from 'qs';
+import { PropData } from 'types';
 
 export const getServerSideProps: GetServerSideProps = async (ctx) => {
-    const slug = ctx.params?.slug;
-    const reqDivisionQuery = qs.stringify(
-        {
-            filters: {
-                slug: {
-                    $eq: slug,
-                },
-            },
-        },
-        {encodeValuesOnly: true}
-    );
-
-    const reqDivisionMemberQuery = qs.stringify(
-        {
-            filters: {
-                division: {
-                    slug: {
-                        $eq: slug,
-                    },
-                },
-            },
-            populate: ['profile.photo'],
-        },
-        {encodeValuesOnly: true}
-    );
-
     try {
-        const reqDivision = await fetchGet(
-            process.env.NEXT_PUBLIC_BACKEND_API +
-                '/api/divisions?' +
-                reqDivisionQuery
-        );
-        const resDivision = await reqDivision.json();
+        const slug = ctx.params?.slug;
 
-        const reqDivisionMembers = await fetchGet(
-            process.env.NEXT_PUBLIC_BACKEND_API +
-                '/api/division-members?' +
-                reqDivisionMemberQuery
-        );
-        const resDivisionMembers = await reqDivisionMembers.json();
-
+        if (!slug) {
+            return {
+                notFound: true
+            }
+        }
+    
+        const res = await Promise.all(
+                [
+                    DivisionModel.getDivision(slug.toString()) , 
+                    DivisionMemberModel.getDivisionMembers(slug.toString())
+                ]
+            );
+        
+        console.log('res[0]', res[0]);
+        
         return {
             props: {
-                division: resDivision.data[0],
-                divisionMembers: responseToDivisionMembers(resDivisionMembers),
+                division: {
+                    data: res[0]
+                },
+                divisionMembers: {
+                    data: res[1]
+                },
             },
         };
     } catch (error) {
         return {
             props: {
-                division: [],
-            },
-        };
+                division: {
+                    error: 'Cannot fetch data'
+                },
+                divisionMembers: {
+                    error: 'Cannot fetch data'
+                }
+            }
+        }
     }
+
 };
 
 interface PropsPage {
-    division?: any;
-    divisionMembers?: { data: any[] };
+    division: PropData<Division>;
+    divisionMembers: PropData<DivisionMemberItem[]>;
 }
 
 const DivisionMember: NextPage<PropsPage> = ({division, divisionMembers}) => {
     return (
         <>
-            <h1 className="font-monumentExtended text-center text-2xl">
+            <h1 className="text-center text-2xl">
                 Division
                 <br />
-                {division.name}
+                {division.data?.name}
             </h1>
             <div className="flex flex-col md:flex-row md:justify-center md:flex-wrap p-8 gap-4">
-                {divisionMembers?.data.map((item: any) => (
+                {divisionMembers.data?.map((item) => (
                     <CardDivisionMember
+                        {...item}
                         className="md:flex-shrink-0"
                         key={item.id}
                         title={item.name}
-                        photo={
-                            item.photo ? BACKEND_MEDIA_PREFIX + item.photo : undefined
-                        }
-                        role={item.role}
-                        kelas={item.kelas}
                     />
                 ))}
             </div>
@@ -94,22 +77,3 @@ const DivisionMember: NextPage<PropsPage> = ({division, divisionMembers}) => {
 };
 
 export default DivisionMember;
-
-interface IDivisionMember {
-    id: number,
-    name: string,
-    photo: string,
-    role: string,
-    kelas: string
-}
-
-const responseToDivisionMembers = (res: any) => ({
-    ...res,
-    data: res.data.map((item: any) => ({
-        id: item.id,
-        name: item.profile?.name ? item.profile?.name : null,
-        photo: item.profile?.photo.formats.medium.url ? item.profile?.photo.formats.medium.url : null,
-        role: item.role,
-        kelas: item.profile?.kelas ? item.profile?.kelas : null
-    })),
-});
