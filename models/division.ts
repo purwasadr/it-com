fdd6e7e707148d6a195f97357b81387e771328bf
Purwa Sadr <purwasadr@gmail.com>
2022-08-14 09:10:04 +0700

    refactor: add getDivision and change getDivisions

--- models/division.ts
+++ models/division.ts
@@ -1,19 +1,51 @@
-import {fetchGet} from 'libs/fetch';
+import axios from 'axios';
+import { BACKEND_API } from 'libs/constants';
+import QueryString from 'qs';
+import { toDivision, toDivisions } from 'utils/transform';
 
-export interface Division {
-    id: number;
+export interface Division extends DivisionItem {
+}
+
+export interface DivisionItem {
+    id?: number;
     slug?: string;
     name?: string;
 }
 
-export function getDivisions() {
-    return fetchGet(
-        process.env.NEXT_PUBLIC_BACKEND_API + '/api/divisions'
-    ).then((res) => res.json());
+export async function getDivisions() {
+    const req = await axios.get(
+        BACKEND_API + '/api/divisions'
+    );
+
+    return toDivisions(req.data.data);
+}
+
+export const getDivision = async (slug: string) => {
+    const query = QueryString.stringify(
+        {
+            filters: {
+                slug: {
+                    $eq: slug,
+                },
+            },
+        },
+        {encodeValuesOnly: true}
+    );
+
+    const req = await axios.get(BACKEND_API + '/api/divisions?' + query);
+    const resData = req.data.data[0];
+
+    if (!resData) {
+        throw('Data not found');
+    }
+    
+    return toDivision(resData);
+    
 }
 
 const DivisionModel = {
     getDivisions,
+    getDivision
 };
 
 export default DivisionModel;
