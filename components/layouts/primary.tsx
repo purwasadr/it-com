467f804bc98a3ca3d19cc34a919625121266cb1b
Purwa Sadr <purwasadr@gmail.com>
2022-08-16 10:43:09 +0700

    fix: footer not show when main layout empty

--- components/layouts/primary.tsx
+++ components/layouts/primary.tsx
@@ -18,23 +18,25 @@ const Primary = ({ noPadding = false, children }: Props) => {
                 <meta name="description" content="IT Com adalah website yang dikelola oleh ekskul IT di SMANRA" key="description" />
                 <link rel="icon" href="/favicon.ico" key="icon" />
             </Head>
-            <Navbar/>
-            <main className={`${!noPadding ? 'max-w-6xl min-h-screen py-6 px-4 sm:px-6 md:px-8 mx-auto' : ''}`}>
-                {children}
-            </main>
-            <footer className='px-5 sm:px-8 bg-white border-t'>
-                <span className="flex py-8">
-                    <a className="h-[20px] w-[20px] ml-6" href="">
-                        <TwitterIcon />
-                    </a>
-                    <a className="h-[20px] w-[20px] ml-6" href='https://facebook.com/itcsmanra' target="_blank" rel="noreferrer">
-                        <FacebookIcon />
-                    </a>
-                    <a className="h-[20px] w-[20px] ml-6" href="https://instagram.com/itcom.smanra" target="_blank" rel="noreferrer">
-                        <InstagramIcon />
-                    </a>
-                </span>
-            </footer>
+            <div className="flex flex-col min-h-screen">
+                <Navbar/>
+                <main className={`${!noPadding ? 'max-w-6xl py-6 px-4 sm:px-6 md:px-8 mx-auto flex-1 w-full' : 'flex-1 w-full'}`}>
+                    {children}
+                </main>
+                <footer className='px-5 sm:px-8 bg-white border-t'>
+                    <span className="flex py-8">
+                        <a className="h-[20px] w-[20px] ml-6" href="">
+                            <TwitterIcon />
+                        </a>
+                        <a className="h-[20px] w-[20px] ml-6" href='https://facebook.com/itcsmanra' target="_blank" rel="noreferrer">
+                            <FacebookIcon />
+                        </a>
+                        <a className="h-[20px] w-[20px] ml-6" href="https://instagram.com/itcom.smanra" target="_blank" rel="noreferrer">
+                            <InstagramIcon />
+                        </a>
+                    </span>
+                </footer>
+            </div>
         </>
     )
 }
